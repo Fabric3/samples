e4c50337c34f1e9ed4e785485423218394b1f666
jimmarino <jim.marino@gmail.com>
2010-08-06 00:27:00 +0000

    add Hibernate example

---
+++ features/hibernate/src/main/java/org/fabric3/samples/hibernate/Message.java
@@ -0,0 +1,48 @@
+package org.fabric3.samples.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@Entity
+@XmlRootElement
+public class Message {
+    private Long id;
+    private int version;
+    private String text;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Version
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @XmlElement
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
