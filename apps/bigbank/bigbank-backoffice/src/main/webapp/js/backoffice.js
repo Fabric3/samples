d2962fb292f0122cdd79d712e7f373e3e59df482
jimmarino <jim.marino@gmail.com>
2010-10-15 00:06:40 +0000

    add dynamic charting

--- apps/bigbank/bigbank-backoffice/src/main/webapp/js/backoffice.js
+++ apps/bigbank/bigbank-backoffice/src/main/webapp/js/backoffice.js
@@ -5,12 +5,46 @@ $(document).ready(function() {
     var recordCache;
     var currentLoanId = null;
     var shown = false;
+
+    var connectedEndpoint;
+    var callbackAdded = false;
+
     // initialize the tabs
     var tabs = $("#tabs");
     tabs.tabs();
     tabs.hide();
     $("#details_div").hide();
 
+    var amountData = new Array();
+    var amountSeries = new Array();
+    amountData.push(amountSeries);
+    var amountCounter = 0;
+
+    for (var i = 0; i < 20; i++) {
+        amountSeries.push([amountCounter,0]);
+        amountCounter++;
+    }
+
+    function tickFormatter(x) {
+        return '';
+    }
+
+    function plot(data) {
+        if (amountSeries.length == 20) {
+            amountSeries.shift();
+        }
+        amountSeries.push([amountCounter, data]);
+        amountCounter++;
+        var options = {
+            lines: { show: true },
+            points: { show: true },
+            xaxis: { tickDecimals: 0, tickSize: 1, tickFormatter: tickFormatter},
+            colors:['#2694e8']
+        };
+
+        var placeholder = $("#average_amount_chart");
+        $.plot(placeholder, amountData, options);
+    }
 
     function getElementById() {
         return document.getElementById(arguments[0]);
@@ -170,6 +204,29 @@ $(document).ready(function() {
         authenticated = false;
     }
 
+    function subscribe() {
+        function callback(response) {
+            if (response.transport != 'polling' && response.state != 'connected' && response.state != 'closed') {
+                if (response.status == 200) {
+                    var data = response.responseBody;
+                    if (data.length == 0) {
+                        // no data
+                        return;
+                    }
+                    var message = JSON.parse(data);
+                    plot(message.average);
+                }
+            }
+        }
+
+        var channelAddress = baseUrl + "/channels/StatisticsChannel";
+        var callbackFunction = !callbackAdded ? callback : null;
+        var request = $.atmosphere.request = {transport: 'websocket'};
+        $.atmosphere.subscribe(channelAddress, callbackFunction, request);
+        connectedEndpoint = $.atmosphere.response;
+        callbackAdded = true;
+    }
+
     getElementById('login_btn').onclick = function() {
         if (!authenticated) {
             login();
@@ -188,6 +245,13 @@ $(document).ready(function() {
         $("#details_div").hide();
         return false;
     };
+
+    tabs.bind('tabsshow', function(event, ui) {
+        if (ui.panel.id == "analyst_tab") {
+            subscribe();
+            plot();
+        }
+    });
 
 
 });
