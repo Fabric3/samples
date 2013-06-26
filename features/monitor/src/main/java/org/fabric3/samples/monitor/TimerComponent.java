36b2855f9a91b700487c647dff0805ea6dba35f5
jmarino <jim.marino@gmail.com>
2013-06-27 01:11:40 +0200

    javadoc

--- features/monitor/src/main/java/org/fabric3/samples/monitor/TimerComponent.java
+++ features/monitor/src/main/java/org/fabric3/samples/monitor/TimerComponent.java
@@ -6,13 +6,18 @@ import org.fabric3.api.annotation.monitor.Monitor;
 import org.oasisopen.sca.annotation.Scope;
 
 /**
- *
+ * Demonstrates sending messages to a monitor destination. The timer is executed periodically.
  */
 @Scope("COMPOSITE")
 public class TimerComponent implements Runnable {
     private TimerMonitor monitor;
     private AtomicInteger count;
 
+    /**
+     * Constructor.
+     *
+     * @param monitor the monitor. The monitor sends messages to the <code>ApplicationDestination</code>, which is configured in the composite for this sample.
+     */
     public TimerComponent(@Monitor("ApplicationDestination") TimerMonitor monitor) {
         this.monitor = monitor;
         count = new AtomicInteger(0);
--- features/monitor/src/main/java/org/fabric3/samples/monitor/TimerMonitor.java
+++ features/monitor/src/main/java/org/fabric3/samples/monitor/TimerMonitor.java
@@ -4,15 +4,14 @@ import org.fabric3.api.annotation.monitor.Info;
 import org.fabric3.api.annotation.monitor.Severe;
 
 /**
- *
+ * Dispatches messages to a monitor destination.
  */
 public interface TimerMonitor {
 
-    @Severe ("The following error was encountered")
+    @Severe("The following error was encountered")
     void error(Throwable error);
 
     @Info("A message was sent: {0}")
     void message(String message);
-
 
 }
