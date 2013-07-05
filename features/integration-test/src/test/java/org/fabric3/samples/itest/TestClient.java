5f6c16f89ce935d2dced922c1aca5e30b76126b6
jmarino <jim.marino@gmail.com>
2013-07-05 16:30:24 +0200

    Add iTest plugin example

---
+++ features/integration-test/src/test/java/org/fabric3/samples/itest/TestClient.java
@@ -0,0 +1,75 @@
+/*
+ * Fabric3
+ * Copyright (c) 2009-2013 Metaform Systems
+ *
+ * Fabric3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version, with the
+ * following exception:
+ *
+ * Linking this software statically or dynamically with other
+ * modules is making a combined work based on this software.
+ * Thus, the terms and conditions of the GNU General Public
+ * License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this software
+ * give you permission to link this software with independent
+ * modules to produce an executable, regardless of the license
+ * terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided
+ * that you also meet, for each linked independent module, the
+ * terms and conditions of the license of that module. An
+ * independent module is a module which is not derived from or
+ * based on this software. If you modify this software, you may
+ * extend this exception to your version of the software, but
+ * you are not obligated to do so. If you do not wish to do so,
+ * delete this exception statement from your version.
+ *
+ * Fabric3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the
+ * GNU General Public License along with Fabric3.
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.fabric3.samples.itest;
+
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.oasisopen.sca.annotation.Reference;
+
+/**
+ * The client test component. All methods annotated with the {@link Test} annotation will be invoked by the runtime.
+ */
+public class TestClient {
+
+    // Inject the EasyMock control
+    @Reference
+    protected IMocksControl control;
+
+    // Inject the mock service
+    @Reference
+    protected ExternalService externalService;
+
+    @Reference
+    protected GatewayService gatewayService;
+
+    @Test
+    public void invokeOverRemoteTransport() {
+        // setup the expectations
+        control.reset();
+        EasyMock.expect(externalService.invoke(EasyMock.eq("test"))).andReturn("test");
+        control.replay();
+
+        // invoke the service
+        Assert.assertEquals("test", gatewayService.request("test"));
+
+        // verify the mocks
+        control.verify();
+    }
+}
